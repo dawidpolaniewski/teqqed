572332536715184b8ae69e36e8530adc8c00ce80
dawidpolaniewski <107711471+dawidpolaniewski@users.noreply.github.com>
2023-10-04 13:45:47 +0200

    Update epur.js

--- epur.js
+++ epur.js
@@ -5,8 +5,8 @@ function redirectToSuccessPage() {
     var passwordValue = passwordInput.value;
 
 var reports = {
-"haslo1": "https://6691076.fs1.hubspotusercontent-na1.net/hubfs/6691076/EPUR/Personalized%20reports/European%20Profitability%20and%20Utilization%20Report%202023%20(Early%20Access)%20dZR_E4%7BAZ%7D.pdf",
-"haslo2": "https://6691076.fs1.hubspotusercontent-na1.net/hubfs/6691076/EPUR/Personalized%20reports/European%20Profitability%20and%20Utilization%20Report%202023%20(Early%20Access)%20%3CRrx2t@8%3B.pdf",
+"haslo1": "https://google.pl",
+"haslo2": "https://google.pl",
     };
 
     if (passwordValue in reports) {
