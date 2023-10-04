e346b64c823fec79054a4fe282e62fd87b9c2a63
dawidpolaniewski <107711471+dawidpolaniewski@users.noreply.github.com>
2023-10-04 13:47:31 +0200

    Update epur-success.js

--- epur-success.js
+++ epur-success.js
@@ -2,9 +2,9 @@ var reportButton = document.getElementById("downloadLink");
 
 var password = new URLSearchParams(window.location.search).get("report");
 
-var reports = {
-"haslo1": "https://google.pl",
-"haslo2": "https://google.pl",
+    var reports = {
+        "haslo1": "https://example.com/report1.pdf",
+        "haslo2": "https://example.com/report2.pdf",
     };
 
 if (password in reports) {
